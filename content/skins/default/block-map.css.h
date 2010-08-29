46b00af5b49297114f2b40c11bdc71fc9c0dc3f0
u38465639 <u38465639@17c4065a-0e48-4232-9eff-2391e3ce7987>
2010-08-29 21:51:44 +0000

    JOFRR: Add CSS includes to allow per-block CSS JOFRR: Some calendar skinning

---
+++ content/skins/default/block-map.css.h
@@ -0,0 +1,16 @@
+#define MAP_HEIGHT 350px
+#define MAP_WIDTH  350px
+
+/* Maps */
+#block-map-div {
+width:MAP_WIDTH;
+margin-top:10px;
+margin-left:auto;
+margin-right:auto;
+}
+
+.embedded-map {
+height:MAP_HEIGHT;
+width:MAP_WIDTH;
+}
+
---
+++ content/skins/wine/block-map.css.h
@@ -0,0 +1,9 @@
+#define MAP_HEIGHT 500px
+#define MAP_WIDTH  500px
+
+
+/* Maps */
+.embedded-map {
+height:MAP_HEIGHT;
+width:MAP_WIDTH;
+}
