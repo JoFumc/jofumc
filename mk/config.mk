9e4f043afece9a0dac39daad914cdbe165508a35
u38465639 <u38465639@17c4065a-0e48-4232-9eff-2391e3ce7987>
2010-11-17 20:14:44 +0000

    JOFRR: Publish to beta tree

--- mk/config.mk
+++ mk/config.mk
@@ -7,4 +7,4 @@ STAGING_ROOT=$(PROJECT_ROOT)/staging
 # Toplevel of staged content tree
 CONTENT_ROOT=$(STAGING_ROOT)/content
 # Where to copy to the live tree
-PUBLISH_ROOT=/export/webspace/roots/$(PROJECT)
+PUBLISH_ROOT=/export/webspace/roots/$(PROJECT)/beta
