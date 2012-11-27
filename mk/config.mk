34b2a1c788a46a83d63fda0d962b9580acd26ede
PartialGestalt <PartialGestalt@akephart>
2012-11-27 16:09:11 -0600

    [jofumc] Switch to jofumc.org for release builds

--- mk/config.mk
+++ mk/config.mk
@@ -8,18 +8,13 @@ STAGING_ROOT=$(PROJECT_ROOT)/staging
 CONTENT_ROOT=$(STAGING_ROOT)/content
 # Where to copy to the live tree (default is beta)
 PUBLISH_TARGET?=beta
-#PUBLISH_ROOT=/export/webspace/roots/$(PROJECT)/$(PUBLISH_TARGET)
-RELEASE_ROOT=/export/dreamhost/jofumc.net
-BETA_ROOT=/export/dreamhost/beta.jofumc.net
 # RSYNC targets
 BETA_RSYNC=akephart@beta.jofumc.net:/home/akephart/beta.jofumc.net
-RELEASE_RSYNC=akephart@beta.jofumc.net:/home/akephart/jofumc.net
+RELEASE_RSYNC=akephart@jofumc.org:/home/akephart/jofumc.org
 
 ifeq ($(PUBLISH_TARGET),release)
-PUBLISH_ROOT=$(RELEASE_ROOT)
 PUSH_RSYNC=$(RELEASE_RSYNC)
 else
-PUBLISH_ROOT=$(BETA_ROOT)
 PUSH_RSYNC=$(BETA_RSYNC)
 endif
 
